2c21f563c8f439a4dda8d7099fefcae1c86e6ab0
nurealammiaji <nurealam.miaji@gmail.com>
2023-04-08 03:06:03 +0600

    convert button add

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,92 @@
-function objectCalculatorWithHalf(valueOne, valueTwo) {
+function areaCalculatorWithHalf(valueOne, valueTwo) {
 
     // Object Value One
-    let ObjectValueOne = document.getElementById(valueOne);
-    let ObjectValueOneString = ObjectValueOne.value;
-    let ObjectValueOneNumber = parseInt(ObjectValueOneString);
+    let objectValueOne = document.getElementById(valueOne);
+    let objectValueOneString = objectValueOne.value;
+    let objectValueOneNumber = parseInt(objectValueOneString);
 
     // Object Value Two
-    let ObjectValueTwo = document.getElementById(valueTwo);
-    let ObjectValueTwoString = ObjectValueTwo.value;
-    let ObjectValueTwoNumber = parseInt(ObjectValueTwoString);
+    let objectValueTwo = document.getElementById(valueTwo);
+    let objectValueTwoString = objectValueTwo.value;
+    let objectValueTwoNumber = parseInt(objectValueTwoString);
 
     // Object Calculation
-    let objectArea = 0.5 * ObjectValueOneNumber * ObjectValueTwoNumber;
+    let objectArea = 0.5 * objectValueOneNumber * objectValueTwoNumber;
 
     // Creating List
-    let node = document.createElement('li');
-    let textNode = document.createTextNode(objectArea);
-    node.appendChild(textNode);
+    let list = document.createElement('li');
+    let listText = document.createTextNode(objectArea);
+    list.appendChild(listText);
+
+    // Creating Button
+    let button = document.createElement('button');
+    button.innerText = "Convert to m2";
+    button.style.margin = '3px';
+    button.style.padding = '3px'
+    button.style.background = 'blue';
+    button.style.borderRadius = '5px';
+    list.appendChild(button);
 
     // Object Area Display
     let displayList = document.getElementById('display-list');
-    displayList.appendChild(node);
+    displayList.appendChild(list);
+}
+
+function areaCalculator(valueOne, valueTwo) {
+
+    // Object Value One
+    let objectValueOne = document.getElementById(valueOne);
+    let objectValueOneString = objectValueOne.value;
+    let objectValueOneNumber = parseInt(objectValueOneString);
+
+    // Object Value Two
+    let objectValueTwo = document.getElementById(valueTwo);
+    let objectValueTwoString = objectValueTwo.value;
+    let objectValueTwoNumber = parseInt(objectValueTwoString);
+
+    // Object Calculation
+    let objectArea = objectValueOneNumber * objectValueTwoNumber;
+
+    // Creating List
+    let list = document.createElement('li');
+    let listText = document.createTextNode(objectArea);
+    list.appendChild(listText);
+
+    // Creating Button
+    let button = document.createElement('button');
+    button.innerText = "Convert to m2";
+    button.style.margin = '3px';
+    button.style.padding = '3px'
+    button.style.background = 'blue';
+    button.style.borderRadius = '3px';
+    list.appendChild(button);
+
+    // Object Area Display
+    let displayList = document.getElementById('display-list');
+    displayList.appendChild(list);
 }
 
 // Triangle
 document.getElementById('calculate-btn-triangle').addEventListener('click', function(){
-    objectCalculatorWithHalf('triangle-b', 'triangle-h');
+    areaCalculatorWithHalf('triangle-b', 'triangle-h');
 })
 
 // Rhombus
 document.getElementById('calculate-btn-rhombus').addEventListener('click', function(){
-    objectCalculatorWithHalf('rhombus-d1', 'rhombus-d2');
+    areaCalculatorWithHalf('rhombus-d1', 'rhombus-d2');
 })
 
 // Pentagon
 document.getElementById('calculate-btn-pentagon').addEventListener('click', function(){
-    objectCalculatorWithHalf('pentagon-p', 'pentagon-b');
+    areaCalculatorWithHalf('pentagon-p', 'pentagon-b');
+})
+
+// Rectangle
+document.getElementById('calculate-btn-rectangle').addEventListener('click', function(){
+    areaCalculator('rectangle-w', 'rectangle-l');
+})
+
+// Parallelogram
+document.getElementById('calculate-btn-parallelogram').addEventListener('click', function(){
+    areaCalculator('parallelogram-b', 'parallelogram-h');
 })
